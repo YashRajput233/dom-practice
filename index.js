e7361ee4a679cfe1be13e4b2b17f7ec229b8828c
YashRajput233 <yashp7813@gmail.com>
2023-11-08 12:21:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let a=document.querySelector('.heading')
+console.log(a)
+// let b=document.getElementsByTagName('p')
+// console.log(b)
+// let c=document.createElement('p')
+// c.textContent="asdfkfdnf"
+// a.prepend(c)
+
+// let para1=document.querySelector('#para')
+// para1.remove()
+// console.log(para1.parentElement)
+// let newpara= para1.parentElement
+// newpara.removeChild('a')
+
+// let div=document.querySelector('.newdiv')
+// let newpara=div.childElement('a')
+// newpara.remove()
